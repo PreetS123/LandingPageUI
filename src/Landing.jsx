087b9c7075cc2bd7219ae4466d73cf88a2c9702c
PreetS123 <preetbhardwaj517@gmail.com>
2022-11-19 22:26:21 +0530

    about-section

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Navbar } from './Component/Navbar'
+import { About } from './Pages/About'
 
 export const Landing = () => {
   return (
     <>
      <Navbar/>
+     <About />
     </>
   )
 }
---
+++ src/Pages/About.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styles from "../Styles/About.module.css";
+
+export const About = () => {
+  return (
+    <div className={styles.about_main}>
+      <div className={styles.about}>
+        <div>
+          <h2>
+            Want anything to be
+            <br /> easy with <span>LaslesVPN.</span>
+          </h2>
+        </div>
+        <div>
+          <p>
+            Provide a network for all your needs with ease and fun using{" "}
+            <span>LaslesVPN</span> discover interesting features from us.
+          </p>
+        </div>
+        <div className={styles.getStarted} >
+          <button >Get Started</button>
+        </div>
+      </div>
+
+      <div className={styles.imgdiv}>
+        <img src="https://michaellegedza.github.io/LaslesVPN/assets/svg/intro-img.svg" alt="LaslesVPN" />
+      </div>
+    </div>
+  );
+};
