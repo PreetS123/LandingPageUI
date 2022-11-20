33dad9e6ef7610bcdc5af42ea12c3694614c580f
PreetS123 <preetbhardwaj517@gmail.com>
2022-11-20 12:23:27 +0530

    about-page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import styles from "../Styles/About.module.css";
+import { FaMapMarkerAlt } from "react-icons/fa";
+import {BsMenuAppFill,BsFillPersonFill} from 'react-icons/bs';
+
 
 export const About = () => {
   return (
+    <>
     <div className={styles.about_main}>
       <div className={styles.about}>
         <div>
@@ -26,5 +30,50 @@ export const About = () => {
         <img src="https://michaellegedza.github.io/LaslesVPN/assets/svg/intro-img.svg" alt="LaslesVPN" />
       </div>
     </div>
+
+    <AboutBottom/>
+    </>
   );
 };
+
+
+
+   const AboutBottom=()=>{
+    return(
+      <>
+      <div className={styles.bottom_main}>
+        <div className={styles.bottom_one}>
+        <div className={styles.bottom_icondiv}>
+          <FaMapMarkerAlt className={styles.bottom_icon} />
+        </div>
+        <div>
+          <h4>30+</h4>
+          <p>Location</p>
+        </div>
+        </div>
+          
+        <div className={styles.bottom_one}>
+        <div className={styles.bottom_icondiv}>
+          <BsMenuAppFill className={styles.bottom_icon}/>
+        </div>
+        <div>
+          <h4>50+</h4>
+          <p>Servers</p>
+        </div>
+        </div>
+
+        <div className={styles.bottom_one}>
+        <div className={styles.bottom_icondiv}>
+          <BsFillPersonFill className={styles.bottom_icon}/>
+        </div>
+        <div>
+          <h4>90+</h4>
+          <p>Users</p>
+        </div>
+        </div>
+
+
+      </div>
+      </>
+    )
+  }
