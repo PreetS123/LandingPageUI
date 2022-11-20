7729fec694ca8aaa915190546e1a4eab01ad0614
PreetS123 <preetbhardwaj517@gmail.com>
2022-11-20 14:48:47 +0530

    features-page-done

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Navbar } from './Component/Navbar'
 import { About } from './Pages/About'
+import { Features } from './Pages/Features'
 
 export const Landing = () => {
   return (
     <>
      <Navbar/>
      <About />
+     <Features/>
     </>
   )
 }
--- src/Pages/Features.jsx
+++ src/Pages/Features.jsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import React from 'react';
+import {AiFillCheckCircle} from 'react-icons/ai';
+import styles from '../Styles/Features.module.css';
 
 export const Features = () => {
+
+  const features=[
+    {
+      id:1,
+      content:'Powerfull online protection.'
+    },
+    {
+      id:2,
+      content:'Internet without borders.'
+    },
+    {
+      id:3,
+      content:'Supercharged VPN'
+    },
+    {
+      id:4,
+      content:'No specific time limits.'
+    }
+  ]
   return (
-    <div>Features</div>
+    <>
+       <div className={styles.features_main}>
+        <div className={styles.features_imgbx}>
+          <img  src='https://laslesvpn-landing.netlify.app/images/illustration-2.svg' alt='OUR WEBSITE FEATURES AUR HERE' />
+        </div>
+        <div className={styles.features_second}>
+          <h2>We Provide Many
+             Features You Can Use</h2>
+             <p>You can explore the features that we provide with fun and have their own functions each feature.</p>
+
+             <div>
+              {
+                features.map(item=>{
+                  return (
+                    <div key={item.id} className={styles.features_content}>
+                          <AiFillCheckCircle style={{color:'green',fontSize:'8px'}}/>
+                          <p>{item.content}</p>
+                    </div>
+                  )
+                })
+              }
+             </div>
+        </div>
+       </div>
+    </>
   )
 }
