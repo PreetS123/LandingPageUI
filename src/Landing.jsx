c4d648746bafe98ff0799805d45aedee2e1c1087
PreetS123 <preetbhardwaj517@gmail.com>
2022-11-20 21:26:04 +0530

    landing-page

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-// import { Navbar } from './Component/Navbar'
-import { About } from './Pages/About'
-import { Features } from './Pages/Features'
-import { Pricing } from './Pages/Pricing'
+import React from 'react';
+import { About } from './Pages/About';
+import { Features } from './Pages/Features';
+import { Pricing } from './Pages/Pricing';
+import img_map from './Assets/Huge Global.svg';
+import styles from './Styles/Landing.module.css';
 
 export const Landing = () => {
   return (
@@ -10,6 +11,29 @@ export const Landing = () => {
      <About />
      <Features/>
       <Pricing/>
+      <GlobalNetwork   />
+    </>
+  )
+}
+
+
+const GlobalNetwork=()=>{
+
+  return(
+    <>
+     <div className={styles.global_main} >
+      <div className={styles.global_first} >
+       <div>
+        <h1>Huge Global Network of fast VPN</h1>
+       </div>
+       <div>
+        <p>See <span>LaslesVPN</span> everywhere to make it easier for you when you move locations.</p>
+       </div>
+       </div>
+       <div className={styles.mapimg} >
+           <img src={img_map} alt="" />
+       </div>
+     </div>
     </>
   )
 }
