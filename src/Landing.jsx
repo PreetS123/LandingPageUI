3314050c1dde700dd4f0b663d42db3c0fdc569d6
PreetS123 <preetbhardwaj517@gmail.com>
2022-11-20 21:39:17 +0530

    landing-page

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -4,6 +4,12 @@ import { Features } from './Pages/Features';
 import { Pricing } from './Pages/Pricing';
 import img_map from './Assets/Huge Global.svg';
 import styles from './Styles/Landing.module.css';
+import img_netflix from './Assets/Mask Group (1).svg';
+import img_reddit from './Assets/Mask Group (2).svg';
+import img_amazon from './Assets/Mask Group (2).svg';
+import img_discord from './Assets/Mask Group (3).svg';
+import img_spotify from './Assets/Mask Group (4).svg';
+
 
 export const Landing = () => {
   return (
@@ -33,7 +39,22 @@ const GlobalNetwork=()=>{
        <div className={styles.mapimg} >
            <img src={img_map} alt="" />
        </div>
+       <OurPatners />
      </div>
+    </>
+  )
+}
+
+const OurPatners=()=>{
+  return (
+    <>
+   <div>
+      <img src={img_netflix} alt="" />
+      <img src={img_reddit} alt="" />
+      <img src={img_amazon} alt="" />
+      <img src={img_discord} alt="" />
+      <img src={img_spotify} alt="" />
+   </div>
     </>
   )
 }
